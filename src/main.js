0a76156f7aa0a83f9b543ffdbd8b2383be08c9dd
Simon Rüegg <107176736+Vorran@users.noreply.github.com>
2022-12-18 17:02:32 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
